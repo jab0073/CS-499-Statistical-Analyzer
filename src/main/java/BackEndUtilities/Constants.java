3b50c8deda188d8e94ce20f131121704085a997b
Justin Bushue <jab0073@uah.edu>
2022-09-21 11:40:27 -0500

    added classes for missing measures

--- src/main/java/BackEndUtilities/Constants.java
+++ src/main/java/BackEndUtilities/Constants.java
@@ -7,13 +7,18 @@ import java.util.stream.Collectors;
 
 public class Constants {
     public static final String binomial = "binomial distribution";
+    public static final String chi = "chi square";
     public static final String coefficient = "coefficient of variance";
+    public static final String correlation = "correlation coefficient";
     public static final String least = "least square line";
     public static final String mean = "mean";
     public static final String median = "median";
     public static final String mode = "mode";
     public static final String percentiles = "percentiles";
     public static final String probability = "probability distribution";
+    public static final String rank = "rank sum";
+    public static final String sign = "sign test";
+    public static final String spearman = "spearman rank";
     public static final String std = "standard deviation";
     public static final String variance = "variance";
 
---
+++ src/main/java/Measures/RankSum.java
@@ -0,0 +1,21 @@
+package Measures;
+
+import BackEndUtilities.Constants;
+import BackEndUtilities.DataSet;
+import Interfaces.IMeasure;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.math.BigDecimal;
+
+public class RankSum implements IMeasure<BigDecimal> {
+    private static final Logger logger = LogManager.getLogger(IMeasure.class.getName());
+    @Override
+    public BigDecimal function(DataSet inputData) {
+        String name = Constants.rank;
+        logger.debug("Running " + name);
+
+        //TODO: finish
+        return null;
+    }
+}
