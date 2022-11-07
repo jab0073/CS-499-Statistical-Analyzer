8172849bd49a7ed6db80e5912285b2fe6f96deaa
Justin Bushue <jab0073@uah.edu>
2022-11-07 15:29:38 -0600

    removed UDM stuff

--- src/Statistical Analyzer/java/BackEndUtilities/DynamicJavaClassLoader.java
+++ src/Statistical Analyzer/java/BackEndUtilities/DynamicJavaClassLoader.java
@@ -31,29 +31,16 @@ public class DynamicJavaClassLoader {
             logger.debug("No custom measures found.");
             return;
         }
-        for (File file : files) {
-            if (file.isFile()) {
+        for (File javaFile : files) {
+            if (javaFile.isFile()) {
 
                 try {
-                    logger.debug(FilenameUtils.getBaseName(file.getName()) + ": Trying to load.");
-                    String content = Files.readString(file.toPath(), Charset.defaultCharset());
+                    logger.debug(FilenameUtils.getBaseName(javaFile.getName()) + ": Trying to load.");
+                    String content = Files.readString(javaFile.toPath(), Charset.defaultCharset());
 
-                    File javaFile = new File("CustomMeasures/" + FilenameUtils.getBaseName(file.getName()) + ".java");
                     if (javaFile.getParentFile().exists() || javaFile.getParentFile().mkdirs()) {
 
                         try {
-                            Writer writer = null;
-                            try {
-                                writer = new FileWriter(javaFile);
-                                writer.write(content);
-                                writer.flush();
-                                logger.debug(FilenameUtils.getBaseName(file.getName()) + ": content re-written");
-                            } finally {
-                                try {
-                                    writer.close();
-                                } catch (Exception e) {
-                                }
-                            }
 
                             /** Compilation Requirements *********************************************************************************************/
                             DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<JavaFileObject>();
@@ -76,21 +63,21 @@ public class DynamicJavaClassLoader {
                             /********************************************************************************************* Compilation Requirements **/
                             if (task.call()) {
                                 /** Load and execute *************************************************************************************************/
-                                logger.debug(FilenameUtils.getBaseName(file.getName()) + ": compiling");
+                                logger.debug(FilenameUtils.getBaseName(javaFile.getName()) + ": compiling");
                                 // Create a new custom class loader, pointing to the directory that contains the compiled
                                 // classes, this should point to the top of the package structure!
                                 URLClassLoader classLoader = new URLClassLoader(new URL[]{new File("./").toURI().toURL()});
                                 // Load the class from the classloader by name....
-                                Class<?> loadedClass = classLoader.loadClass("CustomMeasures." + FilenameUtils.getBaseName(file.getName()));
+                                Class<?> loadedClass = classLoader.loadClass("CustomMeasures." + FilenameUtils.getBaseName(javaFile.getName()));
                                 // Create a new instance...
                                 Object obj = loadedClass.getDeclaredConstructor().newInstance();
                                 // Santity check
                                 if (obj instanceof IMeasure measure) {
                                     MeasureManager.dynamicallyAddMeasure(measure);
-                                    logger.debug(FilenameUtils.getBaseName(file.getName()) + ": added to measures");
+                                    logger.debug(FilenameUtils.getBaseName(javaFile.getName()) + ": added to measures");
                                 }
                                 else {
-                                    logger.error(FilenameUtils.getBaseName(file.getName()) + ": doesn't implement IMeasure");
+                                    logger.error(FilenameUtils.getBaseName(javaFile.getName()) + ": doesn't implement IMeasure");
                                 }
                                 /************************************************************************************************* Load and execute **/
                             } else {
--- src/Statistical Analyzer/java/Respository/RepositoryManager.java
+++ src/Statistical Analyzer/java/Respository/RepositoryManager.java
@@ -21,34 +21,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RepositoryManager {
-    public static List<UserDefinedMeasure> loadedUserDefinedMeasures;
     public static List<DataSet> loadedDataSets;
     private static final Logger logger = LogManager.getLogger(RepositoryManager.class.getName());
 
     private static IStorage storage;
 
-    public static List<UserDefinedMeasure> getAllUserDefinedMeasures() {
-        return loadedUserDefinedMeasures;
-    }
-
     public static List<DataSet> getAllDataSets() {
         return loadedDataSets;
-    }
-
-    public static UserDefinedMeasure getUserDefinedMeasureFromStorage(String name) {
-        String folder = getUDMFolderPath();
-        return loadedUserDefinedMeasures.stream().filter(udm->udm.getName().equalsIgnoreCase(name))
-                .findFirst()
-                .orElseGet(()->{
-                    try {
-                        UserDefinedMeasure r= storage.get(name, folder);
-                        loadedUserDefinedMeasures.add(r);
-                        return r;
-                    } catch (Exception e) {
-                        logger.error(e);
-                        return null;
-                    }
-                });
     }
 
     public static DataSet getDataSetFromStorage(String name) {
@@ -67,12 +46,6 @@ public class RepositoryManager {
                 });
     }
 
-    public static UserDefinedMeasure getUserDefinedMeasure(String name) {
-        return loadedUserDefinedMeasures.stream().filter(udm->udm.getName().equalsIgnoreCase(name))
-                .findFirst()
-                .orElse(null);
-    }
-
     public static DataSet getDataSet(String name) {
         // loadedDataSets.forEach(d -> System.out.println(d.getName()));
         return loadedDataSets.stream().filter(ds->ds.getName().equalsIgnoreCase(name))
@@ -80,32 +53,12 @@ public class RepositoryManager {
                 .orElse(null);
     }
 
-    public static UserDefinedMeasure getUserDefinedMeasure(int index) {
-        return loadedUserDefinedMeasures.get(index);
-    }
-
     public static DataSet getDataSet(int index) {
         return loadedDataSets.get(index);
     }
 
-    public static boolean hasUserDefinedMeasure(String name) {
-        return loadedUserDefinedMeasures.stream().map(udm -> udm.getName().equalsIgnoreCase(name)).findAny().isPresent();
-    }
-
     public static boolean hasDataSet(String name) {
         return loadedDataSets.stream().map(ds -> ds.getName().equalsIgnoreCase(name)).findAny().isPresent();
-    }
-
-    public static UserDefinedMeasure putUserDefinedMeasure(UserDefinedMeasure udm, String name) {
-        if (udm == null) throw new AssertionError("This method must be called with a User Defined Measure, even if it is an empty instance");
-        UserDefinedMeasure udm2 = getUserDefinedMeasure(name);  //If the udm already exists, get the instance
-
-        if (udm2 == null) {   //If it does not, make a new one and add it to loaded user defined measures
-            udm2 = udm;
-            loadedUserDefinedMeasures.add(udm2);
-        }
-        storage.put(udm2, getUDMFolderPath());
-        return udm2;
     }
 
     public static DataSet putDataSet(DataSet ds, String name) {
@@ -189,26 +142,11 @@ public class RepositoryManager {
         return null;
     }
 
-    public static void deleteUserDefinedName(String name) {
-        UserDefinedMeasure udm = getUserDefinedMeasure(name);
-        if (udm != null && udm.getExpression() != null){
-            storage.deleteFile(name, getUDMFolderPath());
-            loadedUserDefinedMeasures.remove(udm);
-        }
-    }
-
     public static void deleteDataSet(String name) {
         DataSet ds = getDataSet(name);
         if (ds != null && ds.getAllDataAsDouble() != null){
             storage.deleteFile(name, getDataSetFolderPath());
             loadedDataSets.remove(ds);
-        }
-    }
-
-    public static void removeFromLoadedUserDefinedMeasures(String name) {
-        UserDefinedMeasure udm = getUserDefinedMeasure(name);
-        if (udm != null){
-            loadedUserDefinedMeasures.remove(udm);
         }
     }
 
@@ -220,12 +158,10 @@ public class RepositoryManager {
     }
 
     public static void init() {
-        loadedUserDefinedMeasures = new ArrayList<>();
         loadedDataSets = new ArrayList<>();
         storage = new FileSystemRepository();
         UserSettings.init();
         storage.init();
-        loadedUserDefinedMeasures.addAll(storage.loadUserDefinedMeasures(getUDMFolderPath()));
         loadedDataSets.addAll(storage.loadDataSets(getDataSetFolderPath()));
     }
 }
